71eaaab2cc5d134b2fdc0aafe6a08e2f7ebe700a
Ryan Mendoza <71746816+ryan-mendoza@users.noreply.github.com>
2021-08-10 18:52:57 -0700

    loading gif

--- src/FormControl.js
+++ src/FormControl.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './styles.css';
+import Loading from './loading.gif';
 
 class FormControl extends Component {
   state = {
@@ -7,27 +8,38 @@ class FormControl extends Component {
   };
 
   handleChange = (e) => {
-    this.setState({ value: e.target.value }, () =>
-      this.props.onInputComplete(this.state.value)
-    );
+    this.setState({ value: e.target.value });
   };
 
   render() {
     const { id, label, onInputComplete, ...restProps } = this.props;
+    const { value } = this.state;
     return (
-      <form>
-        <div className='formControl'>
-          <label htmlFor={id}>{label}</label>
-          <input
-            id={id}
-            value={this.state.value}
-            type='email'
-            placeholder='Please enter your email'
-            onChange={this.handleChange}
-            {...restProps}
-          />
-        </div>
-      </form>
+      <div className='reactForm'>
+        <form>
+          <div className='formControl'>
+            {/* <label htmlFor={id}>{label}</label> */}
+            <input
+              id='email'
+              value={this.state.value}
+              type='email'
+              placeholder='Please enter your email'
+              onChange={this.handleChange}
+              // {...restProps}
+            />
+          </div>
+        </form>
+        {value.trim().length > 0 ? (
+          <output>input value {value} updated!</output>
+        ) : (
+          <output>
+            <br />
+            waiting for input
+            <br />
+            <img src={Loading} height={50} />
+          </output>
+        )}
+      </div>
     );
   }
 }
